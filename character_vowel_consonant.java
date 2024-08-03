bb7372abe42d4878874f8e34463b99e473e2120d
Aman Gupta <amang954817@gmail.com>
2024-08-03 14:52:46 +0530

    Add files via upload

---
+++ character_vowel_consonant.java
@@ -0,0 +1,12 @@
+public class character_vowel_consonant {
+    public static void main(String[] args) {
+        char ch='C';
+        if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U'){
+            System.out.println("vowel");
+        }
+        else if(ch!='a' && ch!='e' && ch!='i' && ch!='o' && ch!='u' && ch!='a' && ch!='e' && ch!='i' && ch!='o' && ch!='u'){
+            System.out.println("consonant");
+        }
+    }
+    
+}
